659a70fc2ce70bb7ac2d19f21955e34372d30d15
yujin <ruy203@gmail.com>
2024-01-22 19:40:48 +0900

    #12909 올바른 괄호

---
+++ 12909.playground/Contents.swift
@@ -0,0 +1,21 @@
+// 올바른 괄호
+
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+  var stack: [Character] = []
+  
+  for char in s {
+    if char == "(" {
+      stack.append(char)
+    } else {
+      if stack.isEmpty { return false }
+      stack.removeLast()
+    }
+  }
+  
+  return stack.isEmpty
+}
+
+print(solution("()()"))
