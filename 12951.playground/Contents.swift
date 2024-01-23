3ba9fb419d3f3f2a1fb0d8a57aaee14c4b3abe4c
yujin <ruy203@gmail.com>
2024-01-23 15:44:16 +0900

    #12951 복습

--- 12951.playground/Contents.swift
+++ 12951.playground/Contents.swift
@@ -2,20 +2,66 @@
 
 import UIKit
 
+//func solution(_ s:String) -> String {
+//  let slist = s.lowercased().map { String($0) }
+//  var result = slist[0].uppercased()
+//  
+//  
+//  for i in 1..<slist.count {
+//    if slist[i - 1] == " " {
+//      result += slist[i].uppercased()
+//    } else {
+//      result += slist[i]
+//    }
+//  }
+//  
+//  return result
+//}
+//
+//print(solution("for the last week"))
+
+
+
+
+//func solution(_ s:String) -> String {
+//  var lowerString = s.lowercased().map{String($0)}
+//  var result = lowerString[0].uppercased()
+//  
+//  for i in 1..<lowerString.count {
+//    if lowerString[i - 1] == " " {
+//      result.append(lowerString[i].uppercased())
+//    } else {
+//      result.append(lowerString[i])
+//    }
+//  }
+//  
+//  
+//  return result
+//}
+//
+//print(solution("for the last week"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func solution(_ s:String) -> String {
-  let slist = s.lowercased().map { String($0) }
-  var result = slist[0].uppercased()
-  
-  
-  for i in 1..<slist.count {
-    if slist[i - 1] == " " {
-      result += slist[i].uppercased()
-    } else {
-      result += slist[i]
-    }
-  }
-  
-  return result
+    return ""
 }
 
-print(solution("for the last week"))
--- 12973.playground/Contents.swift
+++ 12973.playground/Contents.swift
@@ -19,3 +19,31 @@ func solution(_ s:String) -> Int{
 }
 
 print(solution("cdcd"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ s:String) -> Int{
+    var answer:Int = -1
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+
+    return answer
+}
