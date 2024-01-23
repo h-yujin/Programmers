7c0c083b53bba11c0715fd7656118c6a599b160a
yujin <ruy203@gmail.com>
2024-01-23 16:33:23 +0900

    #42842 카펫

---
+++ 42842.playground/Contents.swift
@@ -0,0 +1,27 @@
+// 카펫
+
+import Foundation
+
+//func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+//  let tileCount = brown + yellow
+//  let arr = (1...tileCount).filter {
+//    tileCount % $0 == 0
+//  }.sorted(by: >)
+//
+//  var centerValue = arr[arr.count / 2]
+//  if arr.count % 2 == 0 {
+//    return [arr[arr.count / 2 - 1], centerValue]
+//  } else {
+//    return [centerValue, centerValue]
+//  }
+//
+//}
+//
+//
+//print(solution(3004, 2996))
+
+
+
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    return []
+}
