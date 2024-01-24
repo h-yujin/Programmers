b4d3b041ab123c3db5a9631831fad30cdc26c37c
yujin <ruy203@gmail.com>
2024-01-24 20:23:36 +0900

    #12953 N개의 최소공배수

--- 12953.playground/Contents.swift
+++ 12953.playground/Contents.swift
@@ -1,3 +1,63 @@
+/// N개의 최소공배수
+
 import UIKit
 
-var greeting = "Hello, playground"
+func solution(_ arr:[Int]) -> Int {
+  
+  
+  return arr.reduce(1) {lcm($0, $1)}
+}
+
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+  if b == 0 {
+    return a
+  } else {
+    return gcd(b, a % b)
+  }
+}
+
+func lcm(_ a: Int, _ b: Int) -> Int {
+  return a * b / gcd(a, b)
+}
+
+print(solution([2,6,8,14]))
+
+print(lcm(24, 14))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ arr:[Int]) -> Int {
+  return 0
+}
