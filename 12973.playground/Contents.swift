becf473da24e5dc368cbc4ca100a11ffdf90c222
yujin <ruy203@gmail.com>
2024-01-24 18:55:53 +0900

    #12973 짝지어 제거하기 다시풀기

--- 12973.playground/Contents.swift
+++ 12973.playground/Contents.swift
@@ -2,48 +2,57 @@
 
 
 import Foundation
+//
+//func solution(_ s:String) -> Int{
+//  var arr = s.map{String($0)}
+//  var result = [String]()
+//  
+//  for value in arr {
+//    if !result.isEmpty && result.last == value {
+//      result.removeLast()
+//    } else {
+//      result.append(value)
+//    }
+//  }
+//  
+//  return result.isEmpty ? 1 : 0
+//}
+//
+//print(solution("cdcd"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 func solution(_ s:String) -> Int{
-  var arr = s.map{String($0)}
-  var result = [String]()
+  var sList = s.map{ String($0) }
+  var stack = [String]()
   
-  for value in arr {
-    if !result.isEmpty && result.last == value {
-      result.removeLast()
+  
+  for i in 1..<sList.count {
+    if !stack.isEmpty || stack.last == sList[i] {
+      stack.removeLast()
     } else {
-      result.append(value)
+      stack.append(sList[i])
     }
   }
   
-  return result.isEmpty ? 1 : 0
+  return stack.isEmpty ? 1 : 0
 }
 
-print(solution("cdcd"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-func solution(_ s:String) -> Int{
-    var answer:Int = -1
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print("Hello Swift")
-
-    return answer
-}
+print("\(solution("cdcd"))")
