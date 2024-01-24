a0b823636167ee8fe02f54ef0d7222a152a29e33
yujin <ruy203@gmail.com>
2024-01-24 19:54:49 +0900

    #12985 예상 대진표

---
+++ 12985.playground/Contents.swift
@@ -0,0 +1,57 @@
+// 예상 대진표
+import Foundation
+
+func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
+  var answer = 0
+  var a = a
+  var b = b
+  
+  while a != b {
+    a = calculateNum(a)
+    b = calculateNum(b)
+    answer += 1
+  }
+  
+  return answer
+}
+
+func calculateNum(_ num: Int) -> Int {
+  if num % 2 == 0 {
+     return num / 2
+  } else {
+    return (num + 1) / 2
+  }
+}
+
+
+print(solution(8, 4, 7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
+    var answer = 0
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+
+    return answer
+}
--- 42842.playground/Contents.swift
+++ 42842.playground/Contents.swift
@@ -22,6 +22,22 @@ import Foundation
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
     return []
 }
