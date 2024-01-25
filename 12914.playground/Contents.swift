12e64f7f279c47664a6d213051e353b10a0d9dfa
yujin <ruy203@gmail.com>
2024-01-25 18:22:37 +0900

    #12914 멀리뛰기

---
+++ 12914.playground/Contents.swift
@@ -0,0 +1,67 @@
+// 멀리뛰기
+import UIKit
+
+//func solution(_ n:Int) -> Int {
+//  guard n != 1 else {
+//    return 1
+//  }
+//  
+//  var result = [1, 2]
+//  
+//  for i in 2..<n {
+//    result.append((result[i - 2] + result[i - 1]) % 1234567)
+//  }
+//  
+//  return result.last ?? 0
+//}
+//
+//print(solution(1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ n:Int) -> Int {
+    return 0
+}
