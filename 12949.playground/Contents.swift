783f92325603b339c27b552c6641cda3576a8976
yujin <ruy203@gmail.com>
2024-01-30 15:10:15 +0900

    #12949 행렬의 곱셈

---
+++ 12949.playground/Contents.swift
@@ -0,0 +1,55 @@
+// 행렬의 곱셈
+
+import Foundation
+
+//func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+//
+//  var result = [[Int]]()
+//  for index in 0..<arr1.count {
+//    result.append([])
+//    
+//    for i in 0..<arr2[0].count {
+//      var value = 0
+//      for j in 0..<arr2.count {
+//        print("\(index) \(i) \(j)")
+//        value += arr1[index][j] * arr2[j][i]
+//      }
+//      result[index].append(value)
+//    }
+//  }
+//  
+//  
+//  return result
+//}
+//
+//print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    return []
+}
