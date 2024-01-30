4bcc1012db24e6b5bc27faaec3f840f3101177af
yujin <ruy203@gmail.com>
2024-01-30 11:52:43 +0900

    #87390 n^2 배열 자르기 다시 확인

--- 131127.playground/Contents.swift
+++ 131127.playground/Contents.swift
@@ -4,5 +4,40 @@
 import Foundation
 
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    return 0
+  let count = number.reduce(0, +)
+  var result = 0
+  
+  var dic: [String: Int] = [:]
+  for i in 0..<want.count {
+    dic.updateValue(number[i], forKey: want[i])
+  }
+  
+  for i in 0...discount.count - count {
+    if compareProduct(productList: Array(discount[i..<i+count]), wantList: dic) {
+      result += 1
+    }
+  }
+  return result
 }
+
+// 원하는 리스트에서 해당하는 마트 리스트 빼기, 원하는 리스트이 개수가 0이면 그 i를 리턴
+private func compareProduct(productList: [String], wantList: [String: Int]) -> Bool {
+  var wantList = wantList
+  for product in productList {
+    if let value = wantList["\(product)"], value > 0 {
+      wantList["\(product)"] = value - 1
+    }
+  }
+  if wantList.values.reduce(0, +) == 0 {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+//solution(["banana", "apple", "rice", "pork", "pot"], [3, 2, 2, 2, 1], ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"])
+//print(solution(["banana", "apple", "rice", "pork", "pot"], [3, 2, 2, 2, 1], ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]))
+print(solution(["apple"], [10], ["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"]))
+
+
--- 87390.playground/Contents.swift
+++ 87390.playground/Contents.swift
@@ -3,24 +3,53 @@
 import Foundation
 
 func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+  let n = Int64(n)
   var array = [Int]()
   
   for i in left...right {
-    array.append(Int(calculateValue(n, Int(i + 1))))
+    array.append(max(Int(i / n) + 1, Int(i % n + 1)))
   }
   
   
   return array
 }
 
-private func calculateValue(_ n:Int, _ i:Int) -> Int64 {
+private func calculateValue(_ n:Int, _ i:Int) -> Int {
   if i % n == 0 {
-    return Int64(n)
+    return (i % n) + 1
   } else {
-    return Int64(i / n + 1)
+    return (i / n) + 1
   }
 }
 
+//func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+//    let result: [Int] = (Int(left)...Int(right)).map {
+//        max($0 / n + 1, $0 % n + 1)
+//    }
+//    // print("result: \(result)")
+//    
+//    return result
+//}
 
 ////print(solution(3, Int64(2), Int64(5)))
 print(solution(10000000, Int64(99999999999), Int64(99999999999)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+    return []
+}
