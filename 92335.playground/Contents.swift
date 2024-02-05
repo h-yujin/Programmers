a2e6ca8f127a5b2cb29ccc9e84f9815481445a3d
yujin <ruy203@gmail.com>
2024-02-05 17:11:11 +0900

    #92335 k진수에서 소수 개수 구하기

---
+++ 92335.playground/Contents.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+//func solution(_ n:Int, _ k:Int) -> Int {
+//  let nums = String(n, radix: k).split(separator: "0")
+//  
+//  
+//  var result = 0
+//  
+//  for num in nums {
+//    let n = Int(String(num))!
+//    if isPrimeNumber(n: n) {
+//      result += 1
+//    }
+//  }
+//  
+//  return result
+//}
+//
+//func isPrimeNumber(n: Int) -> Bool {
+//  if n < 2 { return false }
+//   for i in 2..<Int(sqrt(Double(n)) + 1) {
+//       if n % i == 0 { return false }
+//   }
+//   return true
+//}
+//
+//
+////solution(437674, 3)
+//solution(110011, 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ n:Int, _ k:Int) -> Int {
+    return -1
+}
