e4183e5a532ce15ca424e7fbd45114139c18a603
yujin <ruy203@gmail.com>
2024-02-06 15:45:47 +0900

    #84512 모음사전

--- 84512.playground/Contents.swift
+++ 84512.playground/Contents.swift
@@ -1,3 +1,69 @@
-import UIKit
+// 모음사전
 
-var greeting = "Hello, playground"
+import Foundation
+
+//func solution(_ word:String) -> Int {
+//  var arr = ["A", "E", "I", "O", "U"]
+//  var result = [String]()
+//  
+//  func dfs(_ str: String) {
+//    result.append(str)
+//    
+//    if str.count == 5 { return }
+//    
+//    for i in 0..<5 {
+//      dfs(str + arr[i])
+//    }
+//    
+//  }
+//  
+//  dfs("")
+//  
+//  return 0
+//  
+//  let alpha = "AEIOU".map { String($0) }
+//  var result = [String]()
+//  
+//  func dfs(_ str: String) {
+//    result.append(str)
+//    
+////    print("result \(result)")
+//    if str.count == 5 { return }
+//    
+//    for i in 0..<5 {
+//      dfs(str + alpha[i])
+//    }
+//  }
+//  
+//  dfs("")
+//  return result.firstIndex(of: word)!
+//}
+
+//solution("AAAAE")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ word:String) -> Int {
+    return 0
+}
