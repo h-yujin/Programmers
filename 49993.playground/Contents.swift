1d6d4ac21bf73543d2c53974fe164aa1a5d4abbd
yujin <ruy203@gmail.com>
2024-02-14 16:43:09 +0900

    #49993 스킬트리

--- 49993.playground/Contents.swift
+++ 49993.playground/Contents.swift
@@ -1,3 +1,49 @@
-import UIKit
+// 스킬트리
 
-var greeting = "Hello, playground"
+import Foundation
+
+//func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+//  let antecedent = Array(String(skill.reversed()))
+//  print(antecedent)
+//  
+//  var count = skill_trees.count
+//  
+//  for skill_tree in skill_trees {
+//    var stack = Array(antecedent)
+//    for char in Array(String(skill_tree)) {
+//      if stack.contains(char) {
+//        if stack.last == char {
+//          stack.removeLast()
+//        } else {
+//          count -= 1
+//          break
+//        }
+//      }
+//    }
+//  }
+//  
+//  return count
+//}
+//
+//solution("CBD", ["BACDE", "CBADF", "AECB", "BDA", "B"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+  return -1
+}
