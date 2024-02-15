01d21c2cffe17d6cf6d0602e86821774b5b6e003
yujin <ruy203@gmail.com>
2024-02-15 13:20:19 +0900

    #132265 롤케이크 자르기

---
+++ 132265.playground/Contents.swift
@@ -0,0 +1,79 @@
+// 롤케이크 자르기
+
+
+//import Foundation
+//
+//func solution(_ topping:[Int]) -> Int {
+//  var result = 0
+//  
+//  var myDic = [Int: Int]()
+//  var broDic = [Int: Int]()
+//  
+////  for i in 0..<topping.count {
+////    if myDic[topping[i]] == nil {
+////      myDic[topping[i]] = 1
+////    } else {
+////      myDic[topping[i]]! += 1
+////    }
+////  }
+//  for top in topping {
+//    myDic[top] = (myDic[top] ?? 0) + 1
+//  }
+//  
+//  for top in topping {
+//    myDic[top] = (myDic[top] ?? 0) - 1
+//    broDic[top] = (myDic[top] ?? 0) + 1
+//    
+//    if myDic[top] == 0 {
+//      myDic.removeValue(forKey: top)
+//    }
+//    
+//    if myDic.count == broDic.count {
+//      result += 1
+//    }
+//    
+//    print("myDic : \(myDic)")
+//    print("broDic : \(broDic)")
+//  }
+//  
+//  
+//  
+//  return result
+//}
+//
+//
+//solution([1, 2, 1, 3, 1, 4, 1, 2])
+////solution([1, 2, 3, 1, 4])
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ topping:[Int]) -> Int {
+    return -1
+}
