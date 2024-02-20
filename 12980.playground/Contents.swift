21d209ef57db497ed0d25128ac7ee55a7607b4e4
yujin <ruy203@gmail.com>
2024-02-20 18:15:48 +0900

    #12980 점프와 순간이동 다시풀기

--- 12980.playground/Contents.swift
+++ 12980.playground/Contents.swift
@@ -1,50 +1,51 @@
 // 점프와 순간 이동
 import Foundation
 
+//func solution(_ n:Int) -> Int {
+//  var ans:Int = 0
+//  
+//  var n = n
+//  while n > 0 {
+//    if n % 2 == 1 {
+//      ans = ans + 1
+//    }
+//    
+//    n = n / 2
+//  }
+//  
+//  return ans
+//}
+//
+//
+//print(solution(5000))
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func solution(_ n:Int) -> Int {
   var ans:Int = 0
   
-  var n = n
-  while n > 0 {
-    if n % 2 == 1 {
-      ans = ans + 1
-    }
-    
-    n = n / 2
-  }
+  
   
   return ans
 }
 
-
-print(solution(5000))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-func solution(_ n:Int) -> Int {
-    var ans:Int = 0
-    
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print("Hello Swift")
-
-    return ans
-}
+print(solution(5))
