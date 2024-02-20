ad18c09d6997a1a2f3c8696b0a98f3dcdd4ba4ef
yujin <ruy203@gmail.com>
2024-02-20 16:57:28 +0900

    #131704 택배상자

---
+++ 131704.playground/Contents.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+//func solution(_ order:[Int]) -> Int {
+//  var stack = [Int]()
+//  var result = 0
+//  var max = 0
+//
+//  for i in order {
+//    if stack.last == i {
+//      result += 1
+//      let _ = stack.removeLast()
+//    } else if max < i {
+//      result += 1
+//      
+//      for j in max+1..<i {
+//        stack.append(j)
+//      }
+//      max = i
+//    } else {
+//      break
+//    }
+//    
+//    print(stack)
+//    print("max \(max)")
+//  }
+//  
+//  return result
+//}
+//
+//
+//solution([4, 3, 1, 2, 5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ order:[Int]) -> Int {
+    return 0
+}
