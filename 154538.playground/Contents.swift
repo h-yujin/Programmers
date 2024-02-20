956b1907f275aa02ae43c70c92eae865eeba974c
yujin <ruy203@gmail.com>
2024-02-20 15:17:56 +0900

    #154538 숫자 변환하기

---
+++ 154538.playground/Contents.swift
@@ -0,0 +1,69 @@
+// 숫자 변환하기
+
+import Foundation
+
+//func solution(_ x:Int, _ y:Int, _ n:Int) -> Int {
+//  var numSet = Set([x])
+//  var result = 0
+//  
+//  while true {
+//    var addSet = Set<Int>()
+//    
+//    if numSet.contains(y) {
+//      return result
+//    }
+//    
+//    for i in numSet {
+//      if i+n <= y {
+//        addSet.insert(i + n)
+//      }
+//      
+//      if i*2 <= y {
+//        addSet.insert(i * 2)
+//      }
+//      
+//      if i*3 <= y {
+//        addSet.insert(i * 3)
+//      }
+//    }
+////    print("addSet\(addSet)")
+////    print("numSet\(numSet)")
+//    
+//    numSet = addSet
+//    result += 1
+//    if numSet.isEmpty {
+//      break
+//    }
+//  }
+//  
+//  return -1
+//}
+//
+//
+////print(solution(10, 40, 5))
+//solution(10, 40, 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ x:Int, _ y:Int, _ n:Int) -> Int {
+    return 0
+}
