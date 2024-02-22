f939d187cd83e12012f083c017074391015bed7a
yujin <ruy203@gmail.com>
2024-02-22 15:32:28 +0900

    #131701  연속 부분 수열 합의 개수 ektlvnfdjqhrl

--- 131701.playground/Contents.swift
+++ 131701.playground/Contents.swift
@@ -2,56 +2,60 @@
 
 import Foundation
 
+//func solution(_ elements:[Int]) -> Int {
+//  var result: Set<Int> = []
+//  
+//  for i in 0..<elements.count {
+//    var sum = 0
+//    for j in 0..<elements.count {
+//      var idx = i + j
+//      
+//      if idx >= elements.count {
+//        idx = idx - elements.count
+//      }
+//      
+//      sum += elements[idx]
+//      result.insert(sum)
+//    }
+//  }
+//  
+//  print(result)
+//  return result.count
+//}
+//
+//
+//print("------------\n\(solution([7,9,1,1,4]))")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func solution(_ elements:[Int]) -> Int {
-  var result: Set<Int> = []
+ 
   
-  for i in 0..<elements.count {
-    var sum = 0
-    for j in 0..<elements.count {
-      var idx = i + j
-      
-      if idx >= elements.count {
-        idx = idx - elements.count
-      }
-      
-      sum += elements[idx]
-      result.insert(sum)
-    }
-  }
-  
-  print(result)
-  return result.count
+  return 0
 }
 
-
-print("------------\n\(solution([7,9,1,1,4]))")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-func solution(_ elements:[Int]) -> Int {
-    return 0
-}
+solution([7, 9, 1, 1, 4])
--- 68936.playground/Contents.swift
+++ 68936.playground/Contents.swift
@@ -1,3 +1,71 @@
-import UIKit
+// 쿼드압축 후 개수 세기
 
-var greeting = "Hello, playground"
+import Foundation
+
+//var zeroCount = 0
+//var oneCount = 0
+//
+//func dfs(_ arr: [[Int]], _ row: Int, _ col: Int, _ n: Int) {
+//  let norm = arr[row][col]
+////  print("\(arr) \(row) \(col) \(n)")
+//  
+//  for i in row..<row+n {
+//    for j in col..<col+n {
+//      print("\(norm) \(arr[i][j])")
+//      if norm != arr[i][j] {
+//        
+//        dfs(arr, row, col, n / 2)
+//        dfs(arr, row, col + n / 2, n / 2)
+//        dfs(arr, row + n / 2, col, n / 2)
+//        dfs(arr, row + n / 2, col + n / 2, n / 2)
+//        
+//        return
+//      }
+//      
+//    }
+//  }
+//  
+//  if norm == 1 { oneCount += 1 }
+//  else { zeroCount += 1 }
+//}
+//
+//func solution(_ arr:[[Int]]) -> [Int] {
+//  dfs(arr, 0, 0, arr.count)
+//  
+//  return [zeroCount, oneCount]
+//}
+//
+////solution([[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]])
+//solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ arr:[[Int]]) -> [Int] {
+    return []
+}
