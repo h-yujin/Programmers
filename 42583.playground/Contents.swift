5443462a01ba4fd5c4ea972296632abb79301124
yujin <ruy203@gmail.com>
2024-02-22 11:32:34 +0900

    #42583 다리를 지나는 트럭

---
+++ 42583.playground/Contents.swift
@@ -0,0 +1,67 @@
+// 다리를 지나는 트럭
+
+
+import Foundation
+
+//func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+//  var bridge = Array(repeating: 0, count: bridge_length)
+//  var truck = truck_weights
+//  var time = 0
+//  var bridgeWeight = 0
+//  
+//  while !bridge.isEmpty {
+//    time += 1
+//    
+//    bridgeWeight -= bridge.removeFirst()
+//    
+//    if let t = truck.first {
+//      if t + bridgeWeight <= weight {
+//        bridgeWeight += truck.removeFirst()
+//        bridge.append(t)
+//      } else {
+//        bridge.append(0)
+//      }
+//    }
+//    
+//    print("시간 \(time) === 건너는 트럭 \(bridge)  대기 트럭\(truck)")
+//  }
+//  
+//  
+//  return time
+//}
+//
+//solution(2, 10, [7, 4, 5, 6])
+//solution(100, 100, [10])
+//solution(100, 100, [10,10,10,10,10,10,10,10,10,10])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    return 0
+}
